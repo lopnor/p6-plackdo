2eba2596be7e9497d397641e36c998289b745ca2
Nobuo Danjou <nobuo.danjou@gmail.com>
2011-01-09 12:56:35 +0900

    delegation with role

--- lib/Plackdo/Test/Standalone.pm
+++ lib/Plackdo/Test/Standalone.pm
@@ -1,5 +1,22 @@
 use v6;
 
-role Plackdo::Test::Standalone { }
+role Plackdo::Test::Standalone {
+    use Plackdo::Handler::Standalone;
+    use Plackdo::LWP::UserAgent;
+
+    method test_p6sgi (Block $app, Block $client) {
+        my $pid = fork;
+        if ($pid) {
+            my &cb = sub ($req) {
+                return Plackdo::LWP::UserAgent.new.request($req);
+            };
+            $client(&cb);
+        } else {
+            my $handler = Plackdo::Handler::Standalone.new;
+            $handler.run($app);
+        }
+        run("kill $pid");
+    }
+}
 
 # vim: ft=perl6
--- lib/Plackdo/Test/Standalone/Darwin.pm
+++ lib/Plackdo/Test/Standalone/Darwin.pm
@@ -3,23 +3,6 @@ use Plackdo::Test::Standalone;
 use NativeCall;
 sub fork returns Int is native<libc> { ... }
 
-class Plackdo::Test::Standalone::Darwin does Plackdo::Test::Standalone { 
-    use Plackdo::Handler::Standalone;
-    use Plackdo::LWP::UserAgent;
-
-    method test_p6sgi (Block $app, Block $client) {
-        my $pid = fork;
-        if ($pid) {
-            my &cb = sub ($req) {
-                return Plackdo::LWP::UserAgent.new.request($req);
-            };
-            $client(&cb);
-        } else {
-            my $handler = Plackdo::Handler::Standalone.new;
-            $handler.run($app);
-        }
-        run("kill $pid");
-    }
-}
+class Plackdo::Test::Standalone::Darwin does Plackdo::Test::Standalone { }
 
 # vim: ft=perl6
